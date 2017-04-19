93b835676f1888d0f4b69a7056c87c8de9fa9108
Colby Williams <colbylwilliams@gmail.com>
2017-04-19 17:40:47 -0400

    packaging updates

--- NomadCode.BotFramework.Shared/DirectLine/Domain/Addins/DirectLineClientCredentials.cs
+++ NomadCode.BotFramework.Shared/DirectLine/Domain/Addins/DirectLineClientCredentials.cs
@@ -7,26 +7,26 @@ using Microsoft.Rest;
 
 namespace NomadCode.BotFramework
 {
-    public class DirectLineClientCredentials : ServiceClientCredentials
-    {
-        public string Authorization { get; private set; }
+	public class DirectLineClientCredentials : ServiceClientCredentials
+	{
+		public string Authorization { get; private set; }
 
-        public string Endpoint { get; private set; }
+		public string Endpoint { get; private set; }
 
-        public string Secret { get; private set; }
+		public string Secret { get; private set; }
 
-        public DirectLineClientCredentials (string secret = null, string endpoint = null)
-        {
-            Secret = secret;
-            Authorization = secret;
-            Endpoint = endpoint ?? @"https://directline.botframework.com/";
-        }
+		public DirectLineClientCredentials (string secret = null, string endpoint = null)
+		{
+			Secret = secret;
+			Authorization = secret;
+			Endpoint = endpoint ?? @"https://directline.botframework.com/";
+		}
 
-        public override Task ProcessHttpRequestAsync (HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            request.Headers.Authorization = new AuthenticationHeaderValue ("Bearer", Authorization);
+		public override Task ProcessHttpRequestAsync (HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			request.Headers.Authorization = new AuthenticationHeaderValue ("Bearer", Authorization);
 
-            return base.ProcessHttpRequestAsync (request, cancellationToken);
-        }
-    }
+			return base.ProcessHttpRequestAsync (request, cancellationToken);
+		}
+	}
 }
